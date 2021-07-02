1a6a240c4de19198b00fa93b28e9f9df5da4296e
gerard-payne <github@gerard-payne.net>
2021-07-02 12:46:08 +0200

    initial commit

---
+++ source/Carousel.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+import "./Carousel.css";
+
+function Carousel(props) {
+    console.log("Carousel", props);
+    const slideshow = useRef();
+    const timer = useRef();
+    
+    useEffect(() => {
+        const scroller = slideshow.current;
+        timer.current = setInterval(() => {
+            // the scroller's width x number of slides (-2 = -1 because Arrays, -1 original frame) 
+            let scrollLimit = (scroller.offsetWidth * (scroller.childNodes.length - 2));
+            if(scroller.scrollLeft < scrollLimit) {
+                slideshow.current.scrollBy(scroller.offsetWidth, 0);
+            } else {
+                scroller.scrollTo(0, 0);
+            }
+        }, 3000);
+    }, []);
+
+    return (
+        <div id="carousel" ref={ slideshow }>
+            { props.children }
+        </div>
+    );
+}
+
+export default Carousel;
